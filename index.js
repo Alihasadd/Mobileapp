c12b39f881739efe6fa9b16354b525068a8259da
Ali Vidler <alisonvidler@gmail.com>
2025-03-06 12:20:52 -0500

    Added code adjustment from scrimba

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebas
 import { getDatabase } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-database.js";
 
 const firebaseConfig = {
-    databaseURL: process.env.FIREBASE_DATABASE_KEY
+    databaseURL: process.env.DATABASE_URL
 }
 
-const app = initializeApp(firebaseConfig.firebasedatabasekey)
+const app = initializeApp(firebaseConfig)
 const database = getDatabase(app)
 
-console.log(database)
+console.log(firebaseConfig.databaseURL)
 
 let myLeads = []
 const inputEl = document.getElementById("input-el")
